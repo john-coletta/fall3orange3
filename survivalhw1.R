648519a342c82e9d72724a5443530e07cc62e66d
John Coletta <jbcolett@ncsu.edu>
2018-11-01 09:34:04 -0400

    Added preliminary percents of failures

--- survivalhw1.R
+++ survivalhw1.R
@@ -3,3 +3,30 @@ library(survival)
 library(survminer)
 library(muhaz)
 
+# Read in the data file
+pumps <- read_csv('C:\\Users\\johnb\\OneDrive\\Documents\\MSA\\Fall 3\\Survival Analysis\\survivalcsv\\katrina.csv')
+
+head(pumps)
+
+# Percent of pumps that survived
+(1 - mean(pumps$survive))
+
+# Percent for each failure
+# Flood (reason = 1)
+count(pumps[pumps$reason==1,])/count(pumps)
+# Motor (reason = 2)
+count(pumps[pumps$reason==2,])/count(pumps)
+# Surge (reason = 3)
+count(pumps[pumps$reason==3,])/count(pumps)
+# Jammed (reason = 4)
+count(pumps[pumps$reason==4,])/count(pumps)
+
+# Now, of the failures, what are the percent breakdowns
+# Flood (reason = 1)
+count(pumps[pumps$reason==1,])/count(pumps[pumps$survive==0,])
+# Motor (reason = 2)
+count(pumps[pumps$reason==2,])/count(pumps[pumps$survive==0,])
+# Surge (reason = 3)
+count(pumps[pumps$reason==3,])/count(pumps[pumps$survive==0,])
+# Jammed (reason = 4)
+count(pumps[pumps$reason==4,])/count(pumps[pumps$survive==0,])
