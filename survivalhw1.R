eabf7f41ec792bd2ae4cd086b5a2cfb18b3deb0b
John Coletta <jbcolett@ncsu.edu>
2018-11-05 15:47:28 -0500

    Removed new orleans from title

--- survivalhw1.R
+++ survivalhw1.R
@@ -51,7 +51,7 @@ summary(pumps_fit)
 ggsurvplot(pumps_fit, data=pumps, conf.int=F, palette='#377EB8',
            surv.median.line = "hv",
            ggtheme = theme_minimal(), 
-           title="Survival Probability by Hour: All Pumps for New Orleans' Wells",
+           title="Survival Probability by Hour: All Pumps",
            break.x.by=5, xlim=c(0,50),
            xlab="Hour")
 
@@ -74,7 +74,7 @@ ggsurvplot(pumps_reason_no_survive, data=pumps[pumps$reason != 0,], conf.int=F,
            legend.labs=c('Flooded','Motor Failure','Surge','Jammed'),
            surv.median.line = "h",
            ggtheme = theme_minimal(), 
-           title="Survival Probability of Failed Pumps by Failure Condition for New Orleans' Wells",
+           title="Survival Probability of Failed Pumps by Failure Condition",
            break.x.by=5, xlim=c(0,50),
            xlab="Hour")
 
@@ -91,12 +91,12 @@ pumps$hour2 <- ifelse(pumps$hour==48 & pumps$fail==0, 49, pumps$hour)
 pumps_haz <- with(pumps, kphaz.fit(hour2,fail))
 pumps_haz2 <- with(pumps, kphaz.fit(hour,fail))
 
-kphaz.plot(pumps_haz, main = "Hazard Function for New Orleans' Wells")
+kphaz.plot(pumps_haz, main = "Hazard Function for All Pumps")
 kphaz.plot(pumps_haz2, main= 'hazard function')
 # Cumulative hazard plot
 ggsurvplot(pumps_fit, fun = "cumhaz", palette = "#377EB8",
            ggtheme=theme_minimal(),
-           title="Cumulative Hazard Plot for New Orleans' Wells",
+           title="Cumulative Hazard Plot for All Pumps",
            xlab='Hour',ylab='Cumulative Hazard',
            legend.labs='All Wells',
            break.x.by=5, xlim=c(0,50))
@@ -109,7 +109,7 @@ ggsurvplot(pumps_fit, fun = "cumhaz", palette = "#377EB8",
 #            ggtheme=theme_minimal())
 ggsurvplot(pumps_reason_no_survive, fun='cumhaz', palette=brewer.pal(4, "Set1"),
            ggtheme=theme_minimal(),
-           title="Cumulative Hazard Plot by Failure Condition for New Orleans' Wells",
+           title="Cumulative Hazard Plot by Failure Condition for Pumps",
            xlab='Hour',ylab='Cumulative Hazard',
            legend.labs=c('Flooded','Motor Failure','Surge','Jammed'),
            break.x.by=5, xlim=c(0,50))
