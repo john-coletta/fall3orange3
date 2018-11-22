745f7f0d010841449a92ae592fa6e302e5d4fbcd
John Coletta <jbcolett@ncsu.edu>
2018-11-22 10:24:22 -0500

    Initial simulation hw 4 commit

---
+++ sim-hw4.R
@@ -0,0 +1,15 @@
+# Needed Libraries for Analysis #
+library(graphics)
+library(ks)
+library(lubridate)
+library(tidyverse)
+library(triangle)
+library(MASS)
+library(rgl)
+library(readxl)
+# Use for truncated normal
+#install.packages('truncnorm')
+library(truncnorm)
+library(ggplot2)
+library(scales)
+
